010377bc7f06ba4c20730e47c2c02c9be01f074c
corner430 <corner@88.com>
2026-04-07 16:51:57 +0800

    修复：markdown-preview.nvim 配置无效，opts 改为 init
    markdown-preview.nvim 不使用 setup()，lazy.nvim 的 opts 回调不会被触发，
    导致 vim.g.mkdp_* 变量未被设置，远程预览无法正常工作。
    改用 init 确保变量在插件加载前设置。

--- install-lazyvim.sh
+++ install-lazyvim.sh
@@ -478,7 +478,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     optional = true,
-    opts = function()
+    init = function()
       vim.g.mkdp_open_to_the_world = 1
       vim.g.mkdp_open_ip = "${server_ip}"
       vim.g.mkdp_port = 8888
